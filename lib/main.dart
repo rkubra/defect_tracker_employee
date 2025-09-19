9f72b2d52299651176cbac1819be22b778366a52
rkubra <rkubra.it@gmail.com>
2025-09-19 12:10:21 +0530

    updated
    some code are updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
 import 'dashboard_screen.dart';
